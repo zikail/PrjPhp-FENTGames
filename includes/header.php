0d15588815027986f898129132b7465ca13825d9
Raouf <raoumaki0@gmail.com>
2024-03-25 16:29:59 -0400

    Created the main page
    the main page will be the first thing the user will see when opening the website

---
+++ includes/header.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content=" ie=edge">
+    <link rel="stylesheet" href="../assets/css/header.css">
+</head>
+<body>
+    <header class="top">
+
+        <!-- NAVIGATION BAR | MENU -->
+        <h2 class="logo">TEAM 3</h2> 
+        <nav class="navigation">
+            <a href="../pages/index.php">Home</a> 
+            <a href="../about_page/index_about_page.html">About</a>  
+            <button class="btnSignUp" href="index_about_page.html">Sign Up</button>
+        </nav>
+    
+    
+    </header>
+</body>
+</html>
---
+++ pages/index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content=" ie=edge">
+    <link rel="stylesheet" href="../assets/css/style.css">
+    <link rel="icon" type="image/png" href="../assets/images/logo.png">
+    <title>Fent Games</title>
+</head>
+<body>
+    <?php include('../includes/header.php');?>
+
+    <div class="container">
+        <div class="char section">
+        <br>
+            <img src="../assets/images/char3.png" alt="games">
+            <br>
+        </div>
+            
+        <div class="intro section" style="margin-top: 40px;">
+            <br>
+            <h1 class="title">Welcome</h1>
+            <h1 class="title"> To Fent Games</h1>
+            <br><br><br>
+            <h2 class="subtitle">Where you can find</h2>
+            
+            <h2 class="subtitle"> the best games</h2>
+            <br><br><br>
+            
+            <button class="playBtn">Play Now</button>
+            <br><br><br>
+            <h2 class="subtitle">Unleash</h2>
+            <h2 class="subtitle">Your Potential</h2>
+        </div>
+        <div class="char section">
+            <br>
+            <img src="../assets/images/char1.png" alt="games">
+            <br>
+        </div>
+
+        <?php include('../includes/footer.php');?>
+        
+       
+</body>
+</html>
