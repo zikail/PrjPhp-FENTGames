122a1a99d8a7de0d6c1eefedf47ffcef053a7471
Ariel Bendahan <ariellielrey@hotmail.com>
2024-03-27 08:54:12 -0400

    Created question.css to update question styling

--- pages/questions.php
+++ pages/questions.php
@@ -5,42 +5,46 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../assets/css/style.css">
+    <link rel="stylesheet" href="../assets/css/question.css">
     <link rel="icon" type="image/png" href="../assets/images/logo.png">
     <title>Fent Games</title>
 </head>
 
 <body>
-    <?php
-        $livesCounter = 6;
-        $scoreCounter = 0;
-    
-        // Generate 6 unique random letters
-        $letters = range('a', 'z');
-        shuffle($letters);
-        $letters = array_slice($letters, 0, 6);
-    
+    <div class="question">
+        <?php
+            $livesCounter = 6;
+            $scoreCounter = 0;
+        
+            // Generate 6 unique random letters
+            $letters = range('a', 'z');
+            shuffle($letters);
+            $letters = array_slice($letters, 0, 6);
+        
 
-        // Randomly decide if the letters should be uppercase or lowercase
-        if (rand(0, 1) == 0) 
-        {
-            $letters = array_map('strtoupper', $letters);
-        }
-    
-        if ($livesCounter == 0) 
-        {
-            die("Game over." . "<br>");
-        }
+            // Randomly decide if the letters should be uppercase or lowercase
+            if (rand(0, 1) == 0) 
+            {
+                $letters = array_map('strtoupper', $letters);
+            }
+        
+            if ($livesCounter == 0) 
+            {
+                die("Game over." . "<br>");
+            }
 
-        echo "Letters: " . implode(" ", $letters) . "<br>";
+            echo "<h2>Letters: " . implode(" ", $letters) . "</h2>";
 
-        echo "Your score: $scoreCounter<br>";
-        echo "Lives left: $livesCounter<br>";
+            echo "<p>Your score: $scoreCounter</p>";
+            echo "<p>Lives left: $livesCounter</p>";
         ?>
-    
-        <form method="post" action="<?php echo $_SERVER["PHP_SELF"];?>">
-            Enter the letters in ascending order: <input type="text" name="input">
-            <input type="submit">
-        </form>
+        <div class="options">
+            <form method="post" action="<?php echo $_SERVER["PHP_SELF"];?>">
+                <label for="answer">Your Answer:</label>
+                <input type="text" id="answer" name="answer">
+                <button>Submit</button>
+            </form>
+        </div>
+    </div>
 </body>
-
 </html>
