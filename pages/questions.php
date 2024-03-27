460edd579d36aec66d1fbc1692e118567e4628ae
Ariel Bendahan <ariellielrey@hotmail.com>
2024-03-27 10:49:04 -0400

    Added a game over screen (pretty empty) and added a $_SESSION function to send score to game over page and note lives until 0

--- pages/questions.php
+++ pages/questions.php
@@ -13,33 +13,47 @@
 <body>
     <div class="question">
         <?php
-            $livesCounter = 6;
-            $scoreCounter = 0;
-        
-            // Generate 6 unique random letters
-            $letters = range('a', 'z');
-            shuffle($letters);
-            $letters = array_slice($letters, 0, 6);
-        
+        session_start();
 
-            // Randomly decide if the letters should be uppercase or lowercase
-            if (rand(0, 1) == 0) 
-            {
-                $letters = array_map('strtoupper', $letters);
-            }
-        
-            if ($livesCounter == 0) 
-            {
-                die("Game over." . "<br>");
-            }
+        if (!isset($_SESSION["livesCounter"])) 
+        {
+            $_SESSION["livesCounter"] = $livesCounter = 6;
+        } 
+        else 
+        {
+            $livesCounter = $_SESSION["livesCounter"];
+        }
 
-            echo "<h2>Letters: " . implode(" ", $letters) . "</h2>";
+        if (!isset($_SESSION["scoreCounter"])) 
+        {
+            $_SESSION["scoreCounter"] = $scoreCounter = 0;
+        }
+        else 
+        {
+            $scoreCounter = $_SESSION["scoreCounter"];
+        }
+        // Generate 6 unique random letters
+        $letters = range('a', 'z');
+        shuffle($letters);
+        $letters = array_slice($letters, 0, 6);
 
-            echo "<p>Your score: $scoreCounter</p>";
-            echo "<p>Lives left: $livesCounter</p>";
+
+        // Randomly decide if the letters should be uppercase or lowercase
+        if (rand(0, 1) == 0) {
+            $letters = array_map('strtoupper', $letters);
+        }
+
+        if ($livesCounter == 0) {
+            header("Location: gameOverFail.php");
+        }
+
+        echo "<h2>Letters: " . implode(" ", $letters) . "</h2>";
+
+        echo "<p>Your score: $scoreCounter</p>";
+        echo "<p>Lives left: $livesCounter</p>";
         ?>
         <div class="options">
-            <form method="post" action="<?php echo $_SERVER["PHP_SELF"];?>">
+            <form method="post" action="<?php echo $_SERVER["PHP_SELF"]; ?>">
                 <label for="answer">Your Answer:</label>
                 <input type="text" id="answer" name="answer">
                 <button>Submit</button>
@@ -47,4 +61,5 @@
         </div>
     </div>
 </body>
+
 </html>
